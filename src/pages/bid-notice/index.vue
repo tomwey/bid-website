7e31f4b6ddb978d706824d6c6e04d027212937ea
tomwey <tomwey@163.com>
2018-12-25 15:24:49 +0800

    ccc

---
+++ src/pages/bid-notice/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home-main-wrap">招标公告</div>
+</template>
+<script>
+export default {
+  name: "bid-notice"
+};
+</script>
+<style lang="scss" scoped>
+</style>
+
+
---
+++ src/pages/bid-plan/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home-main-wrap">招标计划</div>
+</template>
+<script>
+export default {
+  name: "bid-plan"
+};
+</script>
+<style lang="scss" scoped>
+</style>
+
+

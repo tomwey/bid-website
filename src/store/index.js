51f4f67eeaa567386f27da5629c59ec00dd61fae
tomwey <tomwey@163.com>
2018-12-29 16:58:10 +0800

    ccc

---
+++ src/store/index.js
@@ -0,0 +1,25 @@
+import Vuex from 'vuex';
+import Vue from 'vue';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+    state: {
+        supinfo: {},
+        token: null,
+        title: ''
+    },
+    mutations: {
+        login: (state, data) => {
+            localStorage.token = data;
+            state.token = data;
+        },
+        logout: (state) => {
+            localStorage.removeItem('token');
+            state.token = null;
+        },
+        title: (state, data) => {
+            state.title = data;
+        }
+    }
+});
